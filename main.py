a5dc61caeceded79b189ab3083548c2d060763ce
Игорь Плахотнюк <igorplahotnuk@MacBook-Pro-Igor.local>
2026-02-06 21:40:17 +0200

    added small changes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.encoders import jsonable_encoder
 from fastapi import status
 
 
@@ -9,4 +10,11 @@ app = FastAPI()
 @app.get("/")
 def index():
     content = HTMLResponse("<h1>Hello Metanit</h1>", status_code=status.HTTP_200_OK)
-    return content
+    return {"message": "Hello metanit"}
+
+
+@app.get("/about")
+def about():
+    data = {"message": "Hello from about page", "data": "Some data from page about"}
+    json_data = jsonable_encoder(data)
+    return JSONResponse(content=json_data, status_code=status.HTTP_200_OK)
